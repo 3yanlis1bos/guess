245195cdc43def49512c693f5d61716f908c0e9d
3yanlis1bos <3yanlis1bos@gmail.com>
2015-04-03 16:27:39 +0300

    initial commit - try not to vomit

---
+++ GuessData/DialogConstant.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GuessData
+{
+    public class DialogConstant
+    {
+        public const string NAME_DEFAULT = "Aysun";
+    }
+}
---
+++ WebApplication/Models/GuessModels.cs
@@ -0,0 +1,18 @@
+﻿using GuessData;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace WebApplication.Models
+{
+    public class GuessModel 
+    {
+        People person;
+        Statistics statistic;
+    }
+}
